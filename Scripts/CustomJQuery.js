80fd196901ccd448510c272b8667966256dfce79
Adma <ashadpm@gmail.com>
2018-01-01 19:04:35 -0500

    Update CustomJQuery.js

--- Scripts/CustomJQuery.js
+++ Scripts/CustomJQuery.js
@@ -7,21 +7,5 @@
             changeYear: true,
             yearRange: "-60:+0"
         });
-
-    //navbar submenus
-    $('.dropdown-submenu a.test').on("click", function (e) {
-        $(this).next('ul').toggle();
-        e.stopPropagation();
-        e.preventDefault();
-    });
-
-    //jumbotron of index page
-    $('.jumbotron').addClass('jumbotronn');
-
-    //PartialUpdateAjax
-    $('.hidePartialUpdateAjax').click(function () {
-        $('#divPartialUpdateAjax').hide();
-        $('#divProgressbar').addClass('active');
-    });
-});
+ });
 
